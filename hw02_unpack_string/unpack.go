f887edb4127fd86dbccf8fd1776331c5dd94ff01
sbarmin <sbarmin@beeline.ru>
2025-04-03 16:12:55 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -51,10 +51,8 @@ func Unpack(in string) (string, error) {
 	// то проводится анализ второго символа - если это цифра x, то записать первый символ x раз
 	// иначе 1 раз
 	if !firstItemIsSlash {
-		fmt.Println("------1------")
 		secondItem := inRunes[1]
 		if unicode.IsDigit(secondItem) {
-			fmt.Println("------2------")
 			secondItemInt, err := strconv.Atoi(string(secondItem))
 			if err != nil {
 				return "", err
@@ -84,9 +82,9 @@ func Unpack(in string) (string, error) {
 
 		fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", i, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-		previousItem := inRunes[i]
-		previousItemIsDigit := unicode.IsDigit(previousItem)
-		previousItemIsSlash := (previousItem == 92)
+		previousItem := inRunes[i-1]
+		//previousItemIsDigit := unicode.IsDigit(previousItem)
+		//previousItemIsSlash := (previousItem == 92)
 
 		item := inRunes[i]
 		itemIsDigit := unicode.IsDigit(item)
@@ -94,76 +92,124 @@ func Unpack(in string) (string, error) {
 
 		nextItem := inRunes[i+1]
 		nextItemIsDigit := unicode.IsDigit(nextItem)
-		nextItemIsSlash := (nextItem == 92)
+		//nextItemIsSlash := (nextItem == 92)
+
+		//--------------------------------------------------------------------
+
+		// подсчет количества предыдущих символов слеш, следующих подряд
+		countPreviousSlash := 0
+		for j := i - 1; j >= 0; j-- {
+			sItem := inRunes[j]
+			sItemIsSlash := (sItem == 92)
+			if sItemIsSlash {
+				countPreviousSlash = countPreviousSlash + 1
+			} else {
+				break
+			}
+		}
+		// определение экранирован ли текущий символ
+		itemIsSlashed := !(countPreviousSlash%2 == 0)
+
+		//--------------------------------------------------------------------
+
+		/*
+			fmt.Println("++++++++++++++++++++++++++++")
+			fmt.Println("previousItem:        ", string(previousItem))
+			fmt.Println("previousItemIsDigit: ", previousItemIsDigit)
+			fmt.Println("previousItemIsSlash: ", previousItemIsSlash)
+		*/
 
 		fmt.Println("++++++++++++++++++++++++++++")
-
-		fmt.Println("previousItem:        ", string(previousItem))
-		fmt.Println("previousItemIsDigit: ", previousItemIsDigit)
-		fmt.Println("previousItemIsSlash: ", previousItemIsSlash)
-
-		fmt.Println("++++++++++++++++++++++++++++")
-
 		fmt.Println("item:                ", string(item))
 		fmt.Println("itemIsDigit:         ", itemIsDigit)
 		fmt.Println("itemIsSlash:         ", itemIsSlash)
-
+		//fmt.Println("countPreviousSlash:  ", countPreviousSlash)
+		fmt.Println("itemIsSlashed:       ", itemIsSlashed)
 		fmt.Println("++++++++++++++++++++++++++++")
 
-		fmt.Println("nextItem:            ", string(nextItem))
-		fmt.Println("nextItemIsDigit:     ", nextItemIsDigit)
-		fmt.Println("nextItemIsSlash:     ", nextItemIsSlash)
-
-		fmt.Println("++++++++++++++++++++++++++++")
+		/*
+			fmt.Println("nextItem:            ", string(nextItem))
+			fmt.Println("nextItemIsDigit:     ", nextItemIsDigit)
+			fmt.Println("nextItemIsSlash:     ", nextItemIsSlash)
+			fmt.Println("++++++++++++++++++++++++++++")
+		*/
 
 		// отсекаем ошибку цифр, идущих подряд, при условии, что первая цифра не экранирована слэшем
-		if !previousItemIsSlash && itemIsDigit && nextItemIsDigit {
+		if !itemIsSlashed && itemIsDigit && nextItemIsDigit {
 			return "", ErrInvalidString
 		}
 
-		/*
-			if itemIsDigit && !previousItemIsSlash {
-				if itemNextIsDigit {
-					return "", ErrInvalidString
-				} else {
-					continue
-				}
-			}
-		*/
-
-		if itemIsDigit {
-			// предыдущий символ может быть только слэш или не слеш и не цифра
-			if previousItemIsSlash {
+		if itemIsDigit || itemIsSlash {
+			if itemIsSlashed {
 				if nextItemIsDigit {
 					nextItemInt, err := strconv.Atoi(string(nextItem))
+					fmt.Println("-----------001---------", nextItemInt)
+					fmt.Println("previousItem:        ", string(previousItem))
+					fmt.Println("item:                ", string(item))
 					if err != nil {
 						return "", err
 					}
-					sb.WriteString(strings.Repeat(string(previousItem)+string(item), nextItemInt))
-				} else if nextItemIsSlash {
-					sb.WriteString(string(previousItem) + string(item))
+					sb.WriteString(strings.Repeat(string(item), nextItemInt))
+					/*
+						for range nextItemInt {
+							//sb.WriteRune(previousItem)
+							sb.WriteRune(item)
+						}
+					*/
 				} else {
-					sb.WriteString(string(previousItem) + string(item))
+					//sb.WriteRune(previousItem)
+					sb.WriteRune(item)
+				}
+			}
+		}
+
+		if !itemIsDigit && !itemIsSlash {
+			if itemIsSlashed {
+				if nextItemIsDigit {
+					nextItemInt, err := strconv.Atoi(string(nextItem))
+					fmt.Println("-----------002---------", nextItemInt)
+					fmt.Println("previousItem:        ", string(previousItem))
+					fmt.Println("item:                ", string(item))
+					if err != nil {
+						return "", err
+					}
+					//sb.WriteString(strings.Repeat(string(item), nextItemInt))
+
+					for range nextItemInt {
+						sb.WriteRune(previousItem)
+						sb.WriteRune(item)
+					}
+
+				} else {
+					sb.WriteRune(previousItem)
+					sb.WriteRune(item)
 				}
 			} else {
-				continue
+				if nextItemIsDigit {
+					nextItemInt, err := strconv.Atoi(string(nextItem))
+					fmt.Println("-----------003---------", nextItemInt)
+					fmt.Println("previousItem:        ", string(previousItem))
+					fmt.Println("item:                ", string(item))
+					if err != nil {
+						return "", err
+					}
+					//sb.WriteString(strings.Repeat(string(item), nextItemInt))
+
+					for range nextItemInt {
+						sb.WriteRune(item)
+					}
+
+				} else {
+					sb.WriteRune(item)
+				}
 			}
-		} else if itemIsSlash {
-			// предыдущий символ может быть только слэш или не слеш и не цифра
-			if previousItemIsSlash {
-
-			} else {
-				continue
-			}
-		} else {
-			// предыдущий символ может быть только слэш или не слеш и не цифра
-			if previousItemIsSlash {
-
-			} else {
-
-			}
-
 		}
+
+		/*
+			if itemIsSlashed {
+
+			}
+		*/
 
 		// анализ предыдущего символа - если он слеш - то определить, нужно ли экранировать.
 		// экранируется только цифра и слеш
@@ -216,14 +262,6 @@ func Unpack(in string) (string, error) {
 		*/
 		fmt.Println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", i, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 	}
-
-	/*
-		for item := range inRunes {
-			//получить последний элемент из sb
-			itemIsDigit := unicode.IsDigit(item)
-			itemIsSlash := (item == 92)
-		}
-	*/
 
 	/*
 			for i := 2; i < inSize; i++ {
