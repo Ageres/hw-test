5e90fb0786c80b5dc7f4dfceceb03e768adbba55
sbarmin <sbarmin@beeline.ru>
2025-04-05 20:20:57 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,7 +2,6 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 	"unicode"
@@ -27,50 +26,17 @@ func Unpack(in string) (string, error) {
 
 	inRunes := []rune(in)
 	inSize := len(inRunes)
-	var sb strings.Builder
-
-	//--------------------------------------
-
-	fmt.Println("--------------------------------------------01---------------------------------------------")
-
-	fmt.Println("in:                 ", in)
-	fmt.Println("inSize:             ", inSize)
-	fmt.Println("inRunes:            ", inRunes)
-
-	fmt.Println("--------------------------------------------02---------------------------------------------")
-
-	/*
-		j := 0
-		for item := range inRunes {
-			fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", j, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-			fmt.Println("item:               ", item)
-			fmt.Println("item string:        ", fmt.Sprint(item))
-			fmt.Println("item rune string:   ", string(rune(item)))
-			fmt.Println("inRunes[", j, "]:         ", inRunes[j])
-			fmt.Println("inRunes[", j, "] string:  ", string(inRunes[j]))
-			fmt.Println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", j, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-			j++
-		}
-	*/
-
-	// fmt.Println("--------------------------------------------03---------------------------------------------")
-
-	// fmt.Println("--------------------------------------------04---------------------------------------------")
-
-	//--------------------------------------
 
 	//--------------------------------
 	// Первый этап
 	// анализируемая строка содержит 0 символов
 
-	fmt.Println("------------01-----------")
-
 	if inSize == 0 {
 		return "", nil
 	}
 
-	fmt.Println("------------02-----------")
-
+	//--------------------------------
+	// Второй этап
 	// анализ первого символа
 	firstItem := inRunes[0]
 
@@ -79,65 +45,22 @@ func Unpack(in string) (string, error) {
 		return "", ErrInvalidString
 	}
 
-	fmt.Println("------------03-----------")
-
 	// если переданная строка содержит только один символ и это слеш
 	// то вернуть ошибку
 	if inSize == 1 && firstItem == 92 {
 		return "", ErrInvalidString
 	}
 
-	fmt.Println("------------04-----------")
-
-	/*
-		lastItem := inRunes[inSize-1]
-		if lastItem == 1 && firstItem == 92 {
-			return "", ErrInvalidString
-		}
-	*/
-
-	//--------------------------------
-	// Второй этап
-
-	/*	// является ли первый символ спешем
-		firstItemIsSlash := (firstItem == 92)
-
-		// обработка , если анализируемая строка содержит 1 символ
-		if inSize == 1 {
-			if !firstItemIsSlash { // если символ не слеш, то вернуть его в виде строки
-				return string(firstItem), nil
-			}
-			return "", nil // иначе вернуть пустую строку
-		}
-
-		// если первый символ не слеш
-		if !firstItemIsSlash {
-			// то проводится анализ второго символа
-			secondItem := inRunes[1]
-			if unicode.IsDigit(secondItem) { // если второй символ некая цифра x, то записать первый символ x раз
-				secondItemInt, err := strconv.Atoi(string(secondItem))
-				if err != nil {
-					return "", err
-				}
-				sb.WriteString(strings.Repeat(string(firstItem), secondItemInt))
-			} else { // иначе записать первый символ 1 раз
-				sb.WriteRune(firstItem)
-			}
-		}
-	*/
-
-	fmt.Println("------------05-----------")
-
 	//--------------------------------
 	// Третий этап
 	// анализ со второго по предпоследний символов
+	var sb strings.Builder
+
 	outTS, err := processThirdStage(inSize, inRunes)
 	if err != nil {
 		return "", err
 	}
 	sb.WriteString(outTS)
-
-	fmt.Println("------------06-----------")
 
 	//--------------------------------
 	// Четвертый этап
@@ -148,12 +71,7 @@ func Unpack(in string) (string, error) {
 	}
 	sb.WriteString(outFS)
 
-	out := sb.String()
-
-	fmt.Println("out:", out)
-	fmt.Println("--------------------------------------------99---------------------------------------------")
-
-	return out, nil
+	return sb.String(), nil
 }
 
 // выполнение третьего этапа.
@@ -215,27 +133,6 @@ func processThirdStageModuleForOtherSymbolType(
 	itemIsSlashed, nextItemIsDigit bool,
 ) (string, error) {
 	var sb strings.Builder
-
-	// обработка, если текущий символ экранирован
-	/*
-		if itemIsSlashed {
-			return "", ErrInvalidString
-			if nextItemIsDigit { // если следующий символ некая цифра x, то записать комбинацию из слеша и текущего символа x раз
-				nextItemInt, err := strconv.Atoi(string(nextItem))
-				if err != nil {
-					return "", err
-				}
-				for range nextItemInt {
-					sb.WriteString("\\")
-					sb.WriteRune(item)
-				}
-			} else { // если следующий символ не цифра, то записать комбинацию из слеша и текущего символа 1 раз
-				sb.WriteString("\\")
-				sb.WriteRune(item)
-			}
-		}
-	*/
-
 	// обработка, если текущий символ не экранирован
 	if !itemIsSlashed {
 		if nextItemIsDigit { // если следующий символ некая цифра x, то записать текущий символ x раз
@@ -268,8 +165,6 @@ func processFourthStage(inSize int, inRunes []rune) (string, error) {
 		if lastItemIsDigit || lastItemIsSlash {
 			sb.WriteRune(lastItem)
 		} else {
-			// sb.WriteString("\\")
-			// sb.WriteRune(lastItem)
 			return "", ErrInvalidString
 		}
 	}
@@ -277,18 +172,10 @@ func processFourthStage(inSize int, inRunes []rune) (string, error) {
 	if !lastItemIsSlashed {
 		if !lastItemIsDigit && !lastItemIsSlash {
 			sb.WriteRune(lastItem)
-			// return "", ErrInvalidString
 		} else if lastItemIsSlash {
 			return "", ErrInvalidString
 		}
 	}
-
-	/*
-		if !lastItemIsSlashed && lastItemIsSlash {
-			return "", ErrInvalidString
-		}
-	*/
-
 	return sb.String(), nil
 }
 
