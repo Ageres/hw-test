eda3f9fd3c911c76c40755ee957ac87dd737fd1a
sbarmin <sbarmin@beeline.ru>
2025-04-15 04:05:03 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "fmt"
+
 type Key string
 
 type Cache interface {
@@ -12,6 +14,7 @@ type lruCache struct {
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	keys     map[*ListItem]Key
 }
 
 func NewCache(capacity int) Cache {
@@ -19,6 +22,7 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		keys:     make(map[*ListItem]Key, capacity),
 	}
 }
 
@@ -27,19 +31,23 @@ func (l *lruCache) Set(key Key, value any) bool {
 	if !ok {
 		if l.queue.Len() == l.capacity {
 			backListItem := l.queue.Back()
-			for k, v := range l.items {
-				if v == backListItem {
-					delete(l.items, k)
-					break
-				}
-			}
+			backKey := l.keys[backListItem]
+			delete(l.items, backKey)
+			delete(l.keys, backListItem)
 			l.queue.Remove(backListItem)
 		}
 	} else {
+		delete(l.keys, oldListItem)
 		l.queue.Remove(oldListItem)
 	}
 	newListItem := l.queue.PushFront(value)
 	l.items[key] = newListItem
+	l.keys[newListItem] = key
+	fmt.Println("-----------------------150--------------------------")
+	fmt.Println("l.queue.Len():", l.queue.Len())
+	fmt.Println("len(l.items):", len(l.items))
+	fmt.Println("len(l.keys):", len(l.keys))
+	fmt.Println("-----------------------190--------------------------")
 	return ok
 }
 
@@ -48,14 +56,25 @@ func (l *lruCache) Get(key Key) (any, bool) {
 	if !ok {
 		return nil, false
 	}
+	fmt.Println("-----------------------250--------------------------")
+	_, okk := l.keys[oldListItem]
+	fmt.Println("okk:", okk)
+	delete(l.keys, oldListItem)
 	value := oldListItem.Value()
 	l.queue.Remove(oldListItem)
 	newListItem := l.queue.PushFront(value)
 	l.items[key] = newListItem
+	l.keys[newListItem] = key
+	fmt.Println("-----------------------250--------------------------")
+	fmt.Println("l.queue.Len():", l.queue.Len())
+	fmt.Println("len(l.items):", len(l.items))
+	fmt.Println("len(l.keys):", len(l.keys))
+	fmt.Println("-----------------------290--------------------------")
 	return value, ok
 }
 
 func (l *lruCache) Clear() {
 	l.queue = NewList()
 	l.items = make(map[Key]*ListItem, l.capacity)
+	l.keys = make(map[*ListItem]Key, l.capacity)
 }
