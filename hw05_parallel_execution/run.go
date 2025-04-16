010f1a90ae385d4cbc5af8d0987ae6af8c0397cb
sbarmin <sbarmin@beeline.ru>
2025-04-16 19:52:23 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -9,7 +9,12 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+// m < 0 - максимум 0 ошибок
 func Run(tasks []Task, n, m int) error {
 	// Place your code here.
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
