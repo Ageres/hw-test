2700e5d3f1290e5273a19275aba221dfc0184677
sbarmin <sbarmin@beeline.ru>
2025-05-19 11:31:11 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"log"
 	"sync"
 	"sync/atomic"
 )
@@ -18,8 +17,6 @@ type Result struct {
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 // m <= 0 - максимум 0 ошибок.
 func Run(tasks []Task, n, m int) error {
-	log.Println("len(tasks) =", len(tasks), ", n =", n, ", m =", m)
-
 	if m <= 0 {
 		return ErrErrorsLimitExceeded
 	}
@@ -28,8 +25,6 @@ func Run(tasks []Task, n, m int) error {
 	endCh, errCountRef := countErr(resultCh, m)
 	taskCh := getTaskChan(tasks, endCh)
 	work(n, resultCh, endCh, taskCh)
-
-	log.Println("errCount =", errCountRef.Load())
 
 	if int(errCountRef.Load()) >= m {
 		return ErrErrorsLimitExceeded
@@ -41,8 +36,6 @@ func countErr(resultCh <-chan Result, m int) (<-chan struct{}, *atomic.Int32) {
 	endCh := make(chan struct{})
 	var errCount atomic.Int32
 	go func() {
-		log.Println(">>> countErr start gourutine")
-		defer log.Println("<<< countErr end gourutine")
 		for r := range resultCh {
 			err := r.Error
 			if err != nil {
@@ -53,7 +46,6 @@ func countErr(resultCh <-chan Result, m int) (<-chan struct{}, *atomic.Int32) {
 				break
 			}
 		}
-		log.Println("!!! close endCh")
 		close(endCh)
 	}()
 	return endCh, &errCount
@@ -62,54 +54,41 @@ func countErr(resultCh <-chan Result, m int) (<-chan struct{}, *atomic.Int32) {
 func getTaskChan(tasks []Task, endCh <-chan struct{}) <-chan Task {
 	taskCh := make(chan Task)
 	go func() {
-		log.Println(">>> getTaskChan start gourutine")
 		defer close(taskCh)
-		defer log.Println("!!! close taskCh")
-
-		for i, task := range tasks {
+		for _, task := range tasks {
 			select {
 			case taskCh <- task:
-				log.Println("+++ getTaskChan process task", i)
 			case <-endCh:
-				log.Println("<<< getTaskChan end 1 gourutine")
 				return
 			}
 		}
-		log.Println("<<< getTaskChan end 2 gourutine")
 	}()
 	return taskCh
 }
 
 func work(n int, resultCh chan Result, endCh <-chan struct{}, taskCh <-chan Task) {
 	wg := &sync.WaitGroup{}
-	for j := range n {
+	for range n {
 		wg.Add(1)
 		go func() {
-			log.Println(">>> work start gourutine", j)
 			defer wg.Done()
 			for {
 				task, ok := <-taskCh
 				if ok {
-					log.Println("--- work process task gourutine", j)
 					err := task()
 					select {
 					case <-endCh:
-						log.Println("<<< work end 1 gourutine", j)
 						return
 					case resultCh <- Result{
 						Error: err,
 					}:
 					}
 				} else {
-					log.Println("<<< work end 2 gourutine", j)
 					return
 				}
 			}
 		}()
 	}
-
 	wg.Wait()
-
-	log.Println("!!! close resultCh")
 	close(resultCh)
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -3,7 +3,6 @@ package hw05parallelexecution
 import (
 	"errors"
 	"fmt"
-	"log"
 	"math/rand"
 	"sync/atomic"
 	"testing"
@@ -122,70 +121,5 @@ func TestRunMy1(t *testing.T) {
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
-	})
-}
-
-// тест с ошибками для дебага - в процессе исполнения таска выводится сообщение.
-func TestRunMy2(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
-	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
-		tasksCount := 10
-		tasks := make([]Task, 0, tasksCount)
-
-		var runTasksCount int32
-
-		for i := 0; i < tasksCount; i++ {
-			err := fmt.Errorf("error from task %d", i)
-			tasks = append(tasks, func() error {
-				log.Println("process task", i)
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-				atomic.AddInt32(&runTasksCount, 1)
-				return err
-			})
-		}
-
-		workersCount := 4
-		maxErrorsCount := 3
-		err := Run(tasks, workersCount, maxErrorsCount)
-
-		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
-	})
-}
-
-// тест без ошибок для дебага - в процессе исполнения таска выводится сообщение.
-func TestRunMy3(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
-	t.Run("tasks without errors", func(t *testing.T) {
-		tasksCount := 10
-		tasks := make([]Task, 0, tasksCount)
-
-		var runTasksCount int32
-		var sumTime time.Duration
-
-		for i := 0; i < tasksCount; i++ {
-			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-			sumTime += taskSleep
-
-			tasks = append(tasks, func() error {
-				log.Println("process task", i)
-				time.Sleep(taskSleep)
-				atomic.AddInt32(&runTasksCount, 1)
-				return nil
-			})
-		}
-
-		workersCount := 5
-		maxErrorsCount := 1
-
-		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
-		elapsedTime := time.Since(start)
-		require.NoError(t, err)
-
-		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
-		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
 }
