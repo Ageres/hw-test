1c065cf3195f9dcc1c92afde86818df46f71246a
sbarmin <sbarmin@beeline.ru>
2025-06-03 01:19:40 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,8 +1,6 @@
 package hw06pipelineexecution
 
 import (
-	"strconv"
-	"strings"
 	"sync"
 )
 
@@ -19,60 +17,61 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	outCh := make(chan interface{})
 	wg := sync.WaitGroup{}
 
-	/*
-		for inItem := range in {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				var sb strings.Builder
-				for _, stage := range stages {
-					outStage := stage(inItem)
-
-
-					if _, isInt := outStageItem; isInt {
-						outStageItem = strconv.Itoa(outStageItem)
-					}
-					sb.WriteString(outStageItem)
-				}
-			}()
-		}
-	*/
-
-	for _, stage := range stages {
+	/*for item := range in {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			outStage := stage(in)
-			var sb strings.Builder
-			for outStageItemIf := range outStage {
-				//log.Printf("----201---- r: %v, type: %T", r, r)
-				var outStageItemStr string
-				if outStageItemInt, isInt := outStageItemIf.(int); isInt {
-					outStageItemStr = strconv.Itoa(outStageItemInt)
-				} else {
-					outStageItemStr = outStageItemIf.(string)
-				}
-				sb.WriteString(outStageItemStr)
-				/*
-					ri, ok := outStageItemIf.(int)
-					//log.Println("----202---- ok:", ok)
-					if ok {
-						//log.Printf("----203---- ok: true, r: %v, type_r: %T, ri: %v", r, r, ri)
-						rs := strconv.Itoa(ri)
-						outCh <- rs
-					} else {
-						//log.Printf("----204---- ok: false, r: %v, type_r: %T, ri: %v", r, r, ri)
-						outCh <- outStageItemIf.(string)
-					}
-				*/
+			for _, stage := range stages {
+				item = stage(item)
 			}
-			outCh <- sb.String()
+			outCh <- item
 		}()
 	}
+	*/
+
+	/*
+		for _, stage := range stages {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				outStage := stage(in)
+				var sb strings.Builder
+				for outStageItemIf := range outStage {
+					//log.Printf("----201---- r: %v, type: %T", r, r)
+					var outStageItemStr string
+					if outStageItemInt, isInt := outStageItemIf.(int); isInt {
+						outStageItemStr = strconv.Itoa(outStageItemInt)
+					} else {
+						outStageItemStr = outStageItemIf.(string)
+					}
+					sb.WriteString(outStageItemStr)
+					/*
+						ri, ok := outStageItemIf.(int)
+						//log.Println("----202---- ok:", ok)
+						if ok {
+							//log.Printf("----203---- ok: true, r: %v, type_r: %T, ri: %v", r, r, ri)
+							rs := strconv.Itoa(ri)
+							outCh <- rs
+						} else {
+							//log.Printf("----204---- ok: false, r: %v, type_r: %T, ri: %v", r, r, ri)
+							outCh <- outStageItemIf.(string)
+						}
+
+				}
+				outCh <- sb.String()
+			}()
+
+		} */
+
 	go func() {
 		wg.Wait()
 		close(outCh)
 
 	}()
 	return outCh
+}
+
+func process(dataCh chan any, stages ...Stage) (any, []Stage) {
+	stage := stages[0]
+
 }
