5f28267082bcffcd4153563e0c87b8fa0d8bc0a2
sbarmin <sbarmin@beeline.ru>
2025-06-30 21:50:35 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,8 +1,6 @@
 package hw06pipelineexecution
 
-import (
-	"sync"
-)
+import "sync"
 
 type (
 	In  = <-chan interface{}
@@ -13,65 +11,41 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	outCh := make(chan interface{})
+	if len(stages) == 0 {
+		return in
+	}
+
 	wg := sync.WaitGroup{}
 
-	/*for item := range in {
+	current := in
+	for _, stage := range stages {
+		stageInput := make(Bi)
 		wg.Add(1)
-		go func() {
+		go func(input Bi, prev Out) {
 			defer wg.Done()
-			for _, stage := range stages {
-				item = stage(item)
-			}
-			outCh <- item
-		}()
-	}
-	*/
-
-	/*
-		for _, stage := range stages {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				outStage := stage(in)
-				var sb strings.Builder
-				for outStageItemIf := range outStage {
-					//log.Printf("----201---- r: %v, type: %T", r, r)
-					var outStageItemStr string
-					if outStageItemInt, isInt := outStageItemIf.(int); isInt {
-						outStageItemStr = strconv.Itoa(outStageItemInt)
-					} else {
-						outStageItemStr = outStageItemIf.(string)
+			defer close(input)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-prev:
+					if !ok {
+						return
 					}
-					sb.WriteString(outStageItemStr)
-					/*
-						ri, ok := outStageItemIf.(int)
-						//log.Println("----202---- ok:", ok)
-						if ok {
-							//log.Printf("----203---- ok: true, r: %v, type_r: %T, ri: %v", r, r, ri)
-							rs := strconv.Itoa(ri)
-							outCh <- rs
-						} else {
-							//log.Printf("----204---- ok: false, r: %v, type_r: %T, ri: %v", r, r, ri)
-							outCh <- outStageItemIf.(string)
-						}
-
+					select {
+					case <-done:
+						return
+					case input <- v:
+					}
 				}
-				outCh <- sb.String()
-			}()
-
-		} */
+			}
+		}(stageInput, current)
+		current = stage(stageInput)
+	}
 
 	go func() {
 		wg.Wait()
-		close(outCh)
-
 	}()
-	return outCh
-}
 
-func process(dataCh chan any, stages ...Stage) (any, []Stage) {
-	stage := stages[0]
-
+	return current
 }
