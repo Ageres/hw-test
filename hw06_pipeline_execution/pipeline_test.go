508ae44ae9c1be3f3f16872a61864c43adf7800c
sbarmin <sbarmin@beeline.ru>
2025-07-01 12:52:08 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -2,6 +2,7 @@ package hw06pipelineexecution
 
 import (
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -92,7 +93,6 @@ func TestPipeline(t *testing.T) {
 	})
 }
 
-/*
 func TestAllStageStop(t *testing.T) {
 	wg := sync.WaitGroup{}
 	// Stage generator
@@ -148,131 +148,3 @@ func TestAllStageStop(t *testing.T) {
 
 	})
 }
-*/
-
-/*
-func TestPipeline1(t *testing.T) {
-	// Stage generator
-	g := func(stageName string, f func(v interface{}) interface{}) Stage {
-		return func(in In) Out {
-			out := make(Bi)
-			go func() {
-				//defer log.Printf("----002---- end: stageName: %v", stageName)
-				defer close(out)
-				for v := range in {
-					time.Sleep(sleepPerStage)
-					o := f(v)
-					//log.Printf("----001---- v: %v, stageName: %v, type_v: %T,  o: %v, type_o: %T", v, stageName, v, o, o)
-					out <- o
-				}
-			}()
-			return out
-		}
-	}
-
-	stages := []Stage{
-		g("Dummy", func(v interface{}) interface{} { return v }),
-		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
-		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
-		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
-	}
-
-	t.Run("simple case", func(t *testing.T) {
-		in := make(Bi)
-		data := []int{1, 2, 3, 4, 5}
-		//data := []int{1}
-
-		go func() {
-			for _, v := range data {
-				in <- v
-			}
-			close(in)
-		}()
-
-		result := make([]string, 0, 10)
-		start := time.Now()
-		for s := range ExecutePipeline(in, nil, stages...) {
-			//log.Printf("----901---- s: %v, type: %T", s, s)
-
-			result = append(result, s.(string))
-
-		}
-
-		//log.Println("----902---- result:", result)
-
-		elapsed := time.Since(start)
-
-		require.Equal(t, []string{"102", "104", "106", "108", "110"}, result)
-		//require.Equal(t, []string{"1", "2", "3", "4", "5"}, result)
-		require.Less(t,
-			int64(elapsed),
-			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
-			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
-	})
-
-}
-*/
-
-/*
-func TestAllStageStop1(t *testing.T) {
-	wg := sync.WaitGroup{}
-	// Stage generator
-	g := func(stageName string, f func(v interface{}) interface{}) Stage {
-		return func(in In) Out {
-			out := make(Bi)
-			wg.Add(1)
-			go func() {
-				defer log.Println("----1002---- end: stageName = ", stageName)
-				defer wg.Done()
-				defer close(out)
-				for v := range in {
-					time.Sleep(sleepPerStage)
-					o := f(v)
-					log.Printf("----1001---- v: %v, stageName: %v, type_v: %T,  o: %v, type_o: %T", v, stageName, v, o, o)
-					out <- o
-				}
-			}()
-			return out
-		}
-	}
-
-	stages := []Stage{
-		g("Dummy", func(v interface{}) interface{} { return v }),
-		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
-		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
-		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
-	}
-
-	t.Run("done case", func(t *testing.T) {
-		in := make(Bi)
-		done := make(Bi)
-		data := []int{1, 2, 3, 4, 5}
-		//data := []int{1}
-
-		// Abort after 200ms
-		abortDur := sleepPerStage * 2
-		go func() {
-			<-time.After(abortDur)
-			close(done)
-		}()
-
-		go func() {
-			for _, v := range data {
-				in <- v
-			}
-			close(in)
-		}()
-
-		result := make([]string, 0, 10)
-		for s := range ExecutePipeline(in, done, stages...) {
-			log.Println("----1011----: s = ", s)
-			result = append(result, s.(string))
-		}
-		log.Println("----1012----")
-		wg.Wait()
-		log.Println("----1013----")
-		require.Len(t, result, 0)
-		log.Println("----1014----")
-	})
-}
-*/
