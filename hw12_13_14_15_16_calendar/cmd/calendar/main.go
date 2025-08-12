44b77a965ff60128359f87fc475a38da932984dd
sbarmin <sbarmin@beeline.ru>
2025-08-12 20:16:50 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var configFile string
+var configFile string = "/config/config.yaml"
 
 // Корневая команда
 var rootCmd = &cobra.Command{
@@ -18,56 +16,36 @@ var rootCmd = &cobra.Command{
 	Long:  "Приложение \"Календарь\" - сервис для хранения календарных событий и отправки уведомлений.",
 }
 
-func Execute() error {
-	return rootCmd.Execute()
+func Execute() {
+	log.Println("----401----")
+	err := rootCmd.Execute()
+	log.Println("----402----")
+	if err != nil {
+		log.Println("----403----")
+		log.Println(err)
+		os.Exit(1)
+	}
+	log.Println("----404----")
+	initConfig()
+	log.Println("----405----")
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	log.Println("----301----configFile:", configFile)
 	rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file (default is $HOME/config/config.yaml)")
 	log.Println("----302----configFile:", configFile)
-	/*
-		rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
-		rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-		rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-		viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-		viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-		viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
-		viper.SetDefault("license", "apache")
-
-		rootCmd.AddCommand(addCmd)
-		rootCmd.AddCommand(initCmd)
-	*/
 }
 
 func initConfig() {
 	log.Println("----201----configFile:", configFile)
 	if configFile != "" {
 		log.Println("----202----configFile:", configFile)
-		// Use config file from the flag.
-		//viper.SetConfigFile(configFile)
 	} else {
 		log.Println("----203----configFile:", configFile)
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+		configFile = "./config.yaml"
 
-		configFile = home + "/config/config.yaml"
-
-		// Search config in home directory with name ".cobra" (without extension).
-		//viper.AddConfigPath(home + "/config/")
-		log.Println("----204---- home:", home)
-		//viper.SetConfigType("yaml")
-		//viper.SetConfigName("config")
 	}
 	log.Println("----205----configFile:", configFile)
-	//viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
-	log.Println("----206----configFile:", configFile)
 }
 
 func main() {
