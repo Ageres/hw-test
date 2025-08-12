ef5535ed94442dab3f58fa612f12c31add590ca4
sbarmin <sbarmin@beeline.ru>
2025-08-13 02:11:44 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger_test.go
@@ -2,7 +2,6 @@ package logger
 
 import (
 	"bytes"
-	"log"
 	"testing"
 
 	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/model"
@@ -10,7 +9,7 @@ import (
 )
 
 func TestLoggerLevel(t *testing.T) {
-	t.Run("test debug level", func(t *testing.T) {
+	t.Run("test debug level with json format", func(t *testing.T) {
 		var buf bytes.Buffer
 		conf := model.LoggerConf{Level: "DEBUG", Format: "JSON"}
 		logger := New(conf, &buf)
@@ -21,8 +20,24 @@ func TestLoggerLevel(t *testing.T) {
 		logger.Error("error message")
 
 		output := buf.String()
-		log.Println("--------001-------:", output)
 		require.Contains(t, output, `"level":"DEBUG","msg":"debug message"`)
+		require.Contains(t, output, `"level":"INFO","msg":"info message"`)
+		require.Contains(t, output, `"level":"WARN","msg":"warn message"`)
+		require.Contains(t, output, `"level":"ERROR","msg":"error message"`)
+	})
+
+	t.Run("test info level with json format", func(t *testing.T) {
+		var buf bytes.Buffer
+		conf := model.LoggerConf{Level: "INFO", Format: "JSON"}
+		logger := New(conf, &buf)
+
+		logger.Debug("debug message")
+		logger.Info("info message")
+		logger.Warn("warn message")
+		logger.Error("error message")
+
+		output := buf.String()
+		require.NotContains(t, output, `"level":"DEBUG","msg":"debug message"`)
 		require.Contains(t, output, `"level":"INFO","msg":"info message"`)
 		require.Contains(t, output, `"level":"WARN","msg":"warn message"`)
 		require.Contains(t, output, `"level":"ERROR","msg":"error message"`)
