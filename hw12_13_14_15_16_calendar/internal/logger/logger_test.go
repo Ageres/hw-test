b2fcd73c916a4eb9e76646ee2ea5bf67c93a8695
sbarmin <sbarmin@beeline.ru>
2025-08-13 02:00:54 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger_test.go
@@ -1,7 +1,29 @@
 package logger
 
-import "testing"
+import (
+	"bytes"
+	"testing"
 
-func TestLogger(t *testing.T) {
-	// TODO
+	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/model"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoggerLevel(t *testing.T) {
+	t.Run("debug level", func(t *testing.T) {
+		var buf bytes.Buffer
+		conf := model.LoggerConf{Level: "DEBUG", Format: "TEXT"}
+		logger := New(conf, &buf)
+
+		logger.Debug("debug message")
+		logger.Info("info message")
+		logger.Warn("warn message")
+		logger.Error("error message")
+
+		output := buf.String()
+		require.Contains(t, output, "debug message")
+		require.Contains(t, output, "info message")
+		require.Contains(t, output, "warn message")
+		require.Contains(t, output, "error message")
+	})
+
 }
