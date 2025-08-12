81c5a244d2f8ece7ae06a563f84c2470c92b0998
sbarmin <sbarmin@beeline.ru>
2025-08-13 02:16:00 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger_test.go
@@ -43,4 +43,55 @@ func TestLoggerLevel(t *testing.T) {
 		require.Contains(t, output, `"level":"ERROR","msg":"error message"`)
 	})
 
+	t.Run("test warn level with json format", func(t *testing.T) {
+		var buf bytes.Buffer
+		conf := model.LoggerConf{Level: "WARN", Format: "JSON"}
+		logger := New(conf, &buf)
+
+		logger.Debug("debug message")
+		logger.Info("info message")
+		logger.Warn("warn message")
+		logger.Error("error message")
+
+		output := buf.String()
+		require.NotContains(t, output, `"level":"DEBUG","msg":"debug message"`)
+		require.NotContains(t, output, `"level":"INFO","msg":"info message"`)
+		require.Contains(t, output, `"level":"WARN","msg":"warn message"`)
+		require.Contains(t, output, `"level":"ERROR","msg":"error message"`)
+	})
+
+	t.Run("test error level with json format", func(t *testing.T) {
+		var buf bytes.Buffer
+		conf := model.LoggerConf{Level: "ERROR", Format: "JSON"}
+		logger := New(conf, &buf)
+
+		logger.Debug("debug message")
+		logger.Info("info message")
+		logger.Warn("warn message")
+		logger.Error("error message")
+
+		output := buf.String()
+		require.NotContains(t, output, `"level":"DEBUG","msg":"debug message"`)
+		require.NotContains(t, output, `"level":"INFO","msg":"info message"`)
+		require.NotContains(t, output, `"level":"WARN","msg":"warn message"`)
+		require.Contains(t, output, `"level":"ERROR","msg":"error message"`)
+	})
+
+	t.Run("test default level with json format", func(t *testing.T) {
+		var buf bytes.Buffer
+		conf := model.LoggerConf{Level: "UNKNOwN", Format: "JSON"}
+		logger := New(conf, &buf)
+
+		logger.Debug("debug message")
+		logger.Info("info message")
+		logger.Warn("warn message")
+		logger.Error("error message")
+
+		output := buf.String()
+		require.Contains(t, output, `"level":"DEBUG","msg":"debug message"`)
+		require.Contains(t, output, `"level":"INFO","msg":"info message"`)
+		require.Contains(t, output, `"level":"WARN","msg":"warn message"`)
+		require.Contains(t, output, `"level":"ERROR","msg":"error message"`)
+	})
+
 }
