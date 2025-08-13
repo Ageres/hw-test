35407859c2c0c0eac7daaa09f995a03d05c6044b
sbarmin <sbarmin@beeline.ru>
2025-08-13 15:07:31 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_16_calendar/internal/storage/memory/storage.go
@@ -4,15 +4,16 @@ import (
 	"sync"
 
 	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/model"
+	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/storage"
 )
 
-type Storage struct {
-	// TODO
+type MemoryStorage struct {
+	storage.Storage
 	mu sync.RWMutex //nolint:unused
 }
 
-func New(cfgRef *model.StorageConf) *Storage {
-	return &Storage{}
+func New(cfgRef *model.StorageConf) *MemoryStorage {
+	return &MemoryStorage{}
 }
 
 // TODO
--- hw12_13_14_15_16_calendar/internal/storage/storage.go
+++ hw12_13_14_15_16_calendar/internal/storage/storage.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"errors"
 	"time"
+
+	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/model"
 )
 
 var (
@@ -12,13 +14,30 @@ var (
 	ErrEventNotFound = errors.New("no events found")
 )
 
+type Event struct {
+	ID          string //UUID
+	Title       string
+	StartTime   time.Time
+	Duration    time.Duration
+	Description string
+	UserID      string //UUID
+	Reminder    time.Duration
+}
+
 type Storage interface {
-	Get(id string) (Event, error)
 	Add(event Event) error
 	Update(id string, event Event) error
 	Delete(id string) error
 	ListDay(day time.Time) ([]Event, error)
 	ListWeek(start time.Time) ([]Event, error)
 	ListMonth(start time.Time) ([]Event, error)
-	ListByUser(userId string) ([]Event, error)
+}
+
+func (e *Event) ToNotification() *model.Notification {
+	return &model.Notification{
+		ID:        e.ID,
+		Title:     e.Title,
+		StartTime: e.StartTime,
+		UserID:    e.UserID,
+	}
 }
