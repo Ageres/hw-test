c8367574f3b7c8971855324d081ee6b30b8de0dd
sbarmin <sbarmin@beeline.ru>
2025-08-13 12:20:49 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/model/config_model.go
+++ hw12_13_14_15_16_calendar/internal/model/config_model.go
@@ -1,20 +1,46 @@
 package model
 
+//-----------------------------
+//common config model
 type Config struct {
 	Logger  *LoggerConf
 	Storage *StorageConf
 	Http    *HttpConf
 }
 
-type StorageConf struct {
-	Type string
-}
-
+//-----------------------------
+//logger config model
 type LoggerConf struct {
 	Level  string
 	Format string
 }
 
+//-----------------------------
+//storage config model
+type StorageConf struct {
+	Type string
+	PSQL PSQLConfig
+}
+
+type PSQLConfig struct {
+	DSN       string
+	Migration string
+	Pool      PoolConf
+}
+
+type PoolConf struct {
+	Conn ConnConf
+}
+
+type ConnConf struct {
+	MaxOpen     int `yaml:"max_open"`
+	MaxIdle     int `yaml:"max_idle"`
+	MaxLifeTime int `yaml:"max_life_time"`
+	MaxIdleTime int `yaml:"max_idle_time"`
+}
+
+//-----------------------------
+//http config model
 type HttpConf struct {
 	Server *ServerConf
 }
