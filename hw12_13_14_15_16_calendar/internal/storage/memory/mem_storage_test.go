4134f7eecc5ca047ea4d4a7afba5a493fdda34ab
sbarmin <sbarmin@beeline.ru>
2025-08-13 23:05:49 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/storage/memory/mem_storage_test.go
+++ hw12_13_14_15_16_calendar/internal/storage/memory/mem_storage_test.go
@@ -1,7 +1,89 @@
 package memorystorage
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func TestStorage(t *testing.T) {
-	// TODO
+	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/storage"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStorage_Add(t *testing.T) {
+	dto := newTestMemoryStorageDto().buildNewEvents()
+	events := dto.events
+
+	t.Run("add events", func(t *testing.T) {
+		dto.buildNewstorage()
+		require.NoError(t, dto.storage.Add(&events[0]))
+		require.NoError(t, dto.storage.Add(&events[1]))
+		require.NoError(t, dto.storage.Add(&events[2]))
+		require.Len(t, dto.storage.events, 3)
+	})
+
+	t.Run("event duplication error when adding", func(t *testing.T) {
+		dto.buildNewstorage()
+		require.NoError(t, dto.storage.Add(&events[0]))
+
+		err := dto.storage.Add(&events[0])
+		require.ErrorIs(t, err, storage.ErrEventAllreadyExists)
+		require.Len(t, dto.storage.events, 1)
+	})
+
+	t.Run("error date busy when adding", func(t *testing.T) {
+		dto.buildNewstorage()
+		require.NoError(t, dto.storage.Add(&events[0]))
+
+		err := dto.storage.Add(&events[3])
+		require.ErrorIs(t, err, storage.ErrDateBusy)
+		require.Len(t, dto.storage.events, 1)
+	})
+
+}
+
+type TestMemoryStorageDto struct {
+	storage *MemoryStorage
+	events  []storage.Event
+}
+
+func newTestMemoryStorageDto() *TestMemoryStorageDto {
+	return &TestMemoryStorageDto{}
+}
+
+func (dto *TestMemoryStorageDto) buildNewstorage() *TestMemoryStorageDto {
+	dto.storage = NewMemoryStorage().(*MemoryStorage)
+	return dto
+}
+
+func (dto *TestMemoryStorageDto) buildNewEvents() *TestMemoryStorageDto {
+	now := time.Now()
+	userID := uuid.New().String()
+	event0 := storage.Event{
+		Title:       "Event 0",
+		StartTime:   now.Add(1 * time.Hour),
+		Duration:    30 * time.Minute,
+		Description: "Test event 0",
+		UserID:      userID,
+	}
+	event1 := storage.Event{
+		Title:     "Event 1",
+		StartTime: now.Add(2 * time.Hour),
+		Duration:  1 * time.Hour,
+		UserID:    userID,
+	}
+	event2 := storage.Event{
+		Title:     "Event 2 (other user)",
+		StartTime: now.Add(1 * time.Hour), // Конфликт по времени, но другой пользователь
+		Duration:  30 * time.Minute,
+		UserID:    uuid.New().String(),
+	}
+	event3 := storage.Event{
+		Title:       "Event 3",
+		StartTime:   now.Add(50 * time.Minute),
+		Duration:    30 * time.Minute,
+		Description: "Test event 3",
+		UserID:      userID,
+	}
+	dto.events = append(dto.events, event0, event1, event2, event3)
+	return dto
 }
