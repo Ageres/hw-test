9fafa0ac54301343360c1be2a75ca1b312b3a762
sbarmin <sbarmin@beeline.ru>
2025-08-15 01:52:59 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/migrations/00001_init_events.sql
+++ hw12_13_14_15_16_calendar/migrations/00001_init_events.sql
@@ -1,6 +1,7 @@
--- migrations/00001_init_events.up.sql
+-- migrations/00001_init_events.sql
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS btree_gist;
 
 CREATE TABLE events (
     id          UUID        PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -15,3 +16,9 @@ CREATE TABLE events (
 CREATE INDEX user_id_idx ON events (user_id);
 CREATE INDEX start_time_idx ON events (start_time);
 CREATE INDEX user_id_start_time_idx ON events (user_id, start_time);
+
+ALTER TABLE events ADD CONSTRAINT no_time_overlaps
+    EXCLUDE USING gist (
+        user_id WITH =,
+        tstzrange(start_time, start_time + (duration * INTERVAL '1 second')) WITH &&
+    );
