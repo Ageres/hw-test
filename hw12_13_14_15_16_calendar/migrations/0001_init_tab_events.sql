7fb0d64de4df1aa1370a61ac3c3228d786520e82
sbarmin <sbarmin@beeline.ru>
2025-08-16 01:20:10 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/migrations/0001_init_tab_events.sql
+++ hw12_13_14_15_16_calendar/migrations/0001_init_tab_events.sql
@@ -3,6 +3,7 @@
 BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS btree_gist;
 
 CREATE TABLE events (
     id          UUID        PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -14,8 +15,18 @@ CREATE TABLE events (
     reminder    INTEGER     CHECK (reminder IS NULL OR reminder > 0)
 );
 
+-- IMMUTABLE-функция для работы с диапазонами
+CREATE OR REPLACE FUNCTION immutable_tstzrange(start_time TIMESTAMPTZ, duration INTEGER)
+RETURNS TSTZRANGE
+LANGUAGE SQL IMMUTABLE
+AS $$
+    SELECT tstzrange(start_time, start_time + (duration * INTERVAL '1 second'))
+$$;
+
 CREATE INDEX idx_user_id ON events (user_id);
 CREATE INDEX idx_start_time_idx ON events (start_time);
 CREATE INDEX idx_user_id_start_time ON events (user_id, start_time);
+CREATE INDEX idx_events_time_range ON events USING gist 
+    (user_id, immutable_tstzrange(start_time, duration));
 
 COMMIT;
