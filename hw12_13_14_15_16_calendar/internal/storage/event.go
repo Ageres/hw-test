f3f3def456fa05ef8c886b935f568e7045c6cfca
sbarmin <sbarmin@beeline.ru>
2025-08-16 12:25:05 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/storage/event.go
+++ hw12_13_14_15_16_calendar/internal/storage/event.go
@@ -33,97 +33,57 @@ func (e *Event) ToNotification() *model.Notification {
 	}
 }
 
+// проверка двух эвентов на пересечение времени
 func (e *Event) Overlaps(other *Event) bool {
 	end1 := e.StartTime.Add(e.Duration)
 	end2 := other.StartTime.Add(other.Duration)
 	return e.StartTime.Before(end2) && end1.After(other.StartTime)
 }
 
-func (e *Event) FullValidate() error {
-	errMsgs := make([]string, 0, 4)
-
-	err := uuid.Validate(e.ID)
-	if err != nil {
-		errMsgs = append(errMsgs, fmt.Sprintf(ErrEventIdMsgTemplate, err))
-	}
-	if e.Title == "" {
-		errMsgs = append(errMsgs, ErrEmptyTitleMsg)
-	}
-	if e.StartTime.Before(time.Now()) {
-		errMsgs = append(errMsgs, ErrEventTimeIsExpiredMsg)
-	}
-	if e.UserID == "" {
-		errMsgs = append(errMsgs, ErrEmptyUserIdMsg)
-	}
-	return NewStorageError(errMsgs...)
-}
-
-// без валидации ID
-func (e *Event) Validate() error {
-	errMsgs := make([]string, 0, 3)
-	if e.Title == "" {
-		errMsgs = append(errMsgs, ErrEmptyTitleMsg)
-	}
-	if e.StartTime.Before(time.Now()) {
-		errMsgs = append(errMsgs, ErrEventTimeIsExpiredMsg)
-	}
-	if e.UserID == "" {
-		errMsgs = append(errMsgs, ErrEmptyUserIdMsg)
-	}
-	return NewStorageError(errMsgs...)
-}
-
-func (e *Event) GenerateId() {
+func (e *Event) GenerateEventId() {
 	e.ID = uuid.New().String()
-}
-
-func ValidateEventNotNil(e *Event) error {
-	if e == nil {
-		return ErrEventIsNil
-	}
-	return nil
 }
 
 func ValidateEventId(eventId string) error {
 	err := uuid.Validate(eventId)
 	if err != nil {
-		return fmt.Errorf(ErrEventIdWrapTemplate, err)
+		return NewStorageErrorWithCause(ErrFailedValidateEventIdTemplate, err)
 	}
 	return nil
 }
 
-func ValidateEventIdStr(eventId string) string {
-	err := uuid.Validate(eventId)
-	if err != nil {
-		return fmt.Sprintf(ErrEventIdMsgTemplate, err)
+func FullValidateEvent(e *Event) error {
+	if e == nil {
+		return ErrEventIsNil
 	}
-	return ""
+	errMsgs := make([]string, 0, 4)
+	err := uuid.Validate(e.ID)
+	if err != nil {
+		errMsgs = append(errMsgs, fmt.Sprintf(ErrFailedValidateEventIdTemplate, err))
+	}
+	errMsgs = append(errMsgs, e.simpleValidate()...)
+	return NewStorageError(errMsgs...)
 }
 
-func (e *Event) ValidateStr() []string {
+// без валидации ID
+func ValidateEvent(e *Event) error {
+	if e == nil {
+		return ErrEventIsNil
+	}
+	errMsgs := e.simpleValidate()
+	return NewStorageError(errMsgs...)
+}
+
+func (e *Event) simpleValidate() []string {
 	errMsgs := make([]string, 0, 3)
 	if e.Title == "" {
 		errMsgs = append(errMsgs, ErrEmptyTitleMsg)
 	}
-	if e.StartTime.Before(time.Now()) {
+	if e.StartTime.Before(time.Now().Add(1 * time.Minute)) {
 		errMsgs = append(errMsgs, ErrEventTimeIsExpiredMsg)
 	}
 	if e.UserID == "" {
 		errMsgs = append(errMsgs, ErrEmptyUserIdMsg)
 	}
 	return errMsgs
-}
-
-func FullValidateEvent(e *Event) error {
-	if e == nil {
-		return NewStorageError(ErrEventIsNilMsg)
-	}
-	return e.FullValidate()
-}
-
-func ValidateEvent(e *Event) error {
-	if e == nil {
-		return NewStorageError(ErrEventIsNilMsg)
-	}
-	return e.Validate()
 }
