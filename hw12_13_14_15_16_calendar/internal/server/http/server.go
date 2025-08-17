40efd4d911b6fa26d82490eac8d60b37ceed059b
sbarmin <sbarmin@beeline.ru>
2025-08-17 18:57:21 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/internal/server/http/server.go
+++ hw12_13_14_15_16_calendar/internal/server/http/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/logger"
 	lg "github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/logger"
 	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/model"
 )
@@ -15,47 +14,48 @@ type Server interface {
 }
 
 type AppServer struct {
-	httpServer *http.Server
-	app        Application
-	logger     lg.Logger
+	server  *http.Server
+	logger  lg.Logger
+	app     Application
+	address string
 }
 
 type Application interface { // TODO
 }
 
-type AppHandler struct {
-	Method  string                                       `json:"method"`
-	Path    string                                       `json:"path"`
-	Handler func(w http.ResponseWriter, r *http.Request) `json:"-"`
-}
-
 func NewServer(ctx context.Context, httpConf *model.HttpConf, app Application) Server {
 	address := httpConf.Server.GetAddress()
-	loggger := lg.GetLogger(ctx)
-	appServer := AppServer{
-		httpServer: &http.Server{
+
+	s := &AppServer{
+		server: &http.Server{
 			Addr: address,
 		},
-		app:    app,
-		logger: loggger,
+		logger:  lg.GetLogger(ctx),
+		app:     app,
+		address: address,
 	}
-	server := appServer.configureMux()
-	logger.GetLogger(ctx).Info("server configured", map[string]any{"address": address})
-	return server
+
+	s.server.Handler = s.createRouter()
+
+	s.logger.Info("server configured", map[string]any{"address": address})
+	return s
+}
+
+func (s *AppServer) createRouter() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", s.helloHandler)
+	mux.HandleFunc("/", s.methodNotAllowedHandler)
+	return s.loggingMiddleware(mux)
 }
 
 func (s *AppServer) Start(ctx context.Context) error {
-	return s.httpServer.ListenAndServe()
+	s.logger.Info("Starting HTTP server", map[string]any{
+		"address": s.address,
+	})
+	return s.server.ListenAndServe()
 }
 
 func (s *AppServer) Stop(ctx context.Context) error {
-	return s.httpServer.Shutdown(ctx)
-}
-
-func (s *AppServer) configureMux() Server {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", s.helloHandler)
-	mux.HandleFunc("/", s.methodNotAllowed)
-	s.httpServer.Handler = s.loggingMiddleware(mux)
-	return s
+	s.logger.Info("Shutting down HTTP server")
+	return s.server.Shutdown(ctx)
 }
