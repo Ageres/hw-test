d6d5f452bf60320f10f8e7c86c4930cbd987ce0c
sbarmin <sbarmin@beeline.ru>
2025-08-17 19:10:55 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -19,7 +19,7 @@ import (
 
 	//"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/logger"
 	//internalhttp "github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/server/http"
-	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/storage"
+
 	storage_config "github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/storage/config"
 )
 
@@ -40,8 +40,6 @@ func main() {
 	ctx = logger.SetNewLogger(ctx, configRef.Logger, nil)
 
 	storage := storage_config.NewStorage(ctx, configRef.Storage)
-
-	//testStorage(ctx, storage)
 
 	calendar := app.New(ctx, storage)
 
@@ -66,84 +64,4 @@ func main() {
 		os.Exit(1)
 	}
 
-}
-
-func testStorage(ctx context.Context, storage storage.Storage) {
-	log.Println("-----------------------1000-------------------------")
-	timeLocation, _ := time.LoadLocation("Local")
-	timeDay := time.Date(2025, 12, 31, 18, 30, 45, 0, timeLocation)
-	log.Println("timeDay:", timeDay)
-
-	events, err := storage.ListDay(ctx, timeDay)
-	if err != nil {
-		log.Println("-----------------------1400-------------------------")
-		log.Fatal(err)
-	}
-	log.Println("-----------------------1425-------------------------")
-	log.Println(logger.MarshalAny(events))
-
-	log.Println("-----------------------1450-------------------------")
-
-	event := events[0]
-	eventId := "00000000-0000-0000-0000-000000000000"
-	event.ID = eventId
-	//event.ID = "2aeef68f-267d-459d-bda6-c900e27f4afb"
-	//event.UserID = "www"
-	//event.StartTime = event.StartTime.Add(30 * time.Minute)
-	event.StartTime = event.StartTime.Add(3 * time.Hour)
-
-	err = storage.Update(ctx, &event)
-	if err != nil {
-		log.Println("-----------------------1475-------------------------")
-		log.Fatal("err:", err)
-	}
-
-	/*
-
-
-		res, err := storage.Add(ctx, &event)
-
-		log.Println(MarshalAny(res))
-	*/
-
-	log.Println("-----------------------1999-------------------------")
-}
-
-func testStorage_01(ctx context.Context, storage storage.Storage) {
-	log.Println("-----------------------1000-------------------------")
-	timeLocation, _ := time.LoadLocation("Local")
-	timeDay := time.Date(2025, 12, 31, 18, 30, 45, 0, timeLocation)
-	//timeDay := time.Date(2026, 1, 1, 11, 30, 45, 0, timeLocation)
-	//timeDay := time.Date(2025, 12, 1, 11, 30, 45, 0, timeLocation)
-	log.Println("timeDay:", timeDay)
-
-	events, err := storage.ListDay(ctx, timeDay)
-	//events, err := storage.ListWeek(ctx, timeDay)
-	//events, err := storage.ListMonth(ctx, timeDay)
-	//err := storage.Delete(ctx, "bb9ac22c-903f-4529-b4eb-d63c6d3fbb18")
-
-	//event := storage.
-
-	if err != nil {
-		log.Println("-----------------------1400-------------------------")
-		log.Fatal(err)
-	}
-	log.Println("-----------------------1400-------------------------")
-	//log.Println(MarshalAny(events))
-	log.Println("len(events):", len(events))
-
-	log.Println("-----------------------1450-------------------------")
-	event := events[0]
-	eventId := "00000000-0000-0000-0000-000000000000"
-	event.ID = eventId
-	event.StartTime = time.Now().Add(121 * time.Hour)
-	//event.Title = ""
-	res, err := storage.Add(ctx, &event)
-	if err != nil {
-		log.Println("-----------------------1475-------------------------")
-		log.Fatal(err)
-	}
-	log.Println("res:", logger.MarshalAny(res))
-
-	log.Println("-----------------------1999-------------------------")
 }
