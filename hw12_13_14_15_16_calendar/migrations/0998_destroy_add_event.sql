99372f567859c5dc53afe60a24ce6e433af69d47
sbarmin <sbarmin@beeline.ru>
2025-08-18 00:47:58 +0300

    HW12 is completed

--- hw12_13_14_15_16_calendar/migrations/0998_destroy_add_event.sql
+++
@@ -1,9 +0,0 @@
--- удаление хранимой процедуры добавления события
-
-BEGIN;
-DROP FUNCTION IF EXISTS public.add_event;
-DROP INDEX IF EXISTS idx_events_id_user;
-DROP INDEX IF EXISTS idx_events_user_time;
-DROP FUNCTION IF EXISTS immutable_tstzrange;
-DROP EXTENSION IF EXISTS btree_gist;
-COMMIT;
--- hw12_13_14_15_16_calendar/migrations/0999_destroy_tab_events.sql
+++
@@ -1,8 +0,0 @@
--- удаление таблицы событий и ее индексов
-
-BEGIN;
-DROP TABLE IF EXISTS events;
-DROP FUNCTION IF EXISTS immutable_tstzrange;
-DROP EXTENSION IF EXISTS "uuid-ossp";
-DROP EXTENSION IF EXISTS btree_gist;
-COMMIT;
