a05fa905224e869b7cb96e7309d503828ce0dc62
sbarmin <sbarmin@beeline.ru>
2025-08-20 22:40:13 +0300

    HW13 is completed

--- hw12_13_14_15_16_calendar/internal/app/app.go
+++ hw12_13_14_15_16_calendar/internal/app/app.go
@@ -2,28 +2,44 @@ package app
 
 import (
 	"context"
+	"time"
+
+	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/storage"
 )
 
 type App struct { // TODO
+	storage storage.Storage
 }
 
-type Logger interface { // TODO
+type AppResponse struct {
+	Status string
+	Result any
 }
 
-type Storage interface { // TODO
+func New(_ context.Context, storage storage.Storage) *App {
+	return &App{storage: storage}
 }
 
-func New(_ context.Context, _ Storage) *App {
-	return &App{}
+func (a *App) AddEvent(ctx context.Context, eventRef *storage.Event) (*storage.Event, error) {
+	return a.storage.Add(ctx, eventRef)
 }
 
-func (a *App) CreateEvent(_ context.Context, id, title string) error {
-	// TODO
-	_ = id
-	_ = title
-
-	return nil
-	// return a.storage.CreateEvent(storage.Event{ID: id, Title: title})
+func (a *App) UpdateEvent(ctx context.Context, eventRef *storage.Event) error {
+	return a.storage.Update(ctx, eventRef)
 }
 
-// TODO
+func (a *App) DeleteEvent(ctx context.Context, eventId string) error {
+	return a.storage.Delete(ctx, eventId)
+}
+
+func (a *App) ListDayEvents(ctx context.Context, startDay time.Time) ([]storage.Event, error) {
+	return a.storage.ListDay(ctx, startDay)
+}
+
+func (a *App) ListWeekEvents(ctx context.Context, startDay time.Time) ([]storage.Event, error) {
+	return a.storage.ListWeek(ctx, startDay)
+}
+
+func (a *App) ListMonthEvents(ctx context.Context, startDay time.Time) ([]storage.Event, error) {
+	return a.storage.ListMonth(ctx, startDay)
+}
