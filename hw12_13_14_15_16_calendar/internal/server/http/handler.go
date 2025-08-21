6e745a17cae411905a2fdcf086af5e4838cb2f56
sbarmin <sbarmin@beeline.ru>
2025-08-21 23:20:50 +0300

    HW13 is completed

--- hw12_13_14_15_16_calendar/internal/server/http/handler.go
+++ hw12_13_14_15_16_calendar/internal/server/http/handler.go
@@ -1,41 +1,23 @@
 package internalhttp
 
 import (
-	"encoding/json"
-	"io"
-	"log/slog"
 	"net/http"
-	"time"
 )
 
 func (s *AppServer) helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		s.service.MethodNotAllowed(w, r)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, World!"))
 }
 
-func (s *AppServer) methodNotAllowedHandler(w http.ResponseWriter, _ *http.Request) {
-	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-}
-
-type Period string
-
-const (
-	DAY   Period = "day"
-	WEEK  Period = "week"
-	MONTH Period = "month"
-)
-
-type ListRequest struct {
-	Period   Period     `json:"period" binding:"required"`
-	StartDay *time.Time `json:"startDay" binding:"required"`
+func (s *AppServer) methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	s.service.MethodNotAllowed(w, r)
 }
 
 func (s *AppServer) eventHandler(w http.ResponseWriter, r *http.Request) {
-	//ctx := r.Context()
 	httpMethod := r.Method
 	switch httpMethod {
 	case http.MethodGet:
@@ -48,48 +30,10 @@ func (s *AppServer) eventHandler(w http.ResponseWriter, r *http.Request) {
 		s.service.UpdateEvent(w, r)
 		return
 	case http.MethodDelete:
-		/*
-				args := r.URL.Query()
-				eventId := args.Get("eventId")
-				resp, err := s.app.DeleteEvent(ctx, eventId)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-					return
-				}
-				writeResponse(w, resp)
-				w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			default:
-				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		*/
+		s.service.DeleteEvent(w, r)
+		return
+	default:
+		s.service.MethodNotAllowed(w, r)
 		return
 	}
-	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-}
-
-func unmarshalRequestBody[T any](w http.ResponseWriter, r *http.Request) (*T, error) {
-	buf := make([]byte, r.ContentLength)
-	_, err := r.Body.Read(buf)
-	if err != nil && err != io.EOF {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return nil, err
-	}
-	reqRef := new(T)
-	err = json.Unmarshal(buf, reqRef)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return nil, err
-	}
-	return reqRef, nil
-}
-
-func writeResponse[T any](w http.ResponseWriter, resp *T) {
-	resBuf, err := json.Marshal(resp)
-	if err != nil {
-		slog.Error("responce marshal error", "err", err)
-	}
-	_, err = w.Write(resBuf)
-	if err != nil {
-		slog.Error("responce marshal error", "err", err)
-	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 }
