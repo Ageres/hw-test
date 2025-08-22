08be705e88b4328714d50b84e66ca8a175a5e511
sbarmin <sbarmin@beeline.ru>
2025-08-23 00:45:46 +0300

    HW13 is completed

--- hw12_13_14_15_16_calendar/internal/server/grpc/server.go
+++ hw12_13_14_15_16_calendar/internal/server/grpc/server.go
@@ -20,11 +20,6 @@ func (g *GrpcServer) AddEvent(context.Context, *pb.AddEventRequest) (*pb.AddEven
 	panic("unimplemented")
 }
 
-// DeleteEvent implements __.CalendarServer.
-func (g *GrpcServer) DeleteEvent(context.Context, *pb.DeleteEventRequest) (*pb.DeleteEventResponse, error) {
-	panic("unimplemented")
-}
-
 // UpdateEvent implements __.CalendarServer.
 func (g *GrpcServer) UpdateEvent(context.Context, *pb.UpdateEventRequest) (*pb.UpdateEventResponse, error) {
 	panic("unimplemented")
@@ -43,50 +38,48 @@ func NewGrpsServer(ctx context.Context, storage storage.Storage) *GrpcServer {
 	}
 }
 
-//func (s *GrpcServer) mustEmbedUnimplementedCalendarServer() {}
-//func (s *GrpcServer) testEmbeddedByValue()                  {}
-
-func (s *GrpcServer) GetEvent(ctx context.Context, req *pb.GetEventListRequest) (*pb.GetEventListResponse, error) {
+func (g *GrpcServer) GetEvent(ctx context.Context, req *pb.GetEventListRequest) (*pb.GetEventListResponse, error) {
 	ctx = utils.SetRequestIdToCtx(ctx)
-	logger := s.logger.With(map[string]any{"requestId": utils.GetRequestID(ctx)})
+	logger := g.logger.With(map[string]any{"requestId": utils.GetRequestID(ctx)})
 	ctx = logger.SetLoggerToCtx(ctx)
-	//req.StartTime = nil
-	if req.StartTime == nil {
-		err := s.createError(ctx, http.StatusBadRequest, "start_time is required", nil)
+
+	startTime := req.GetStartTime()
+	if startTime == nil {
+		err := g.createError(ctx, http.StatusBadRequest, "start_time is required", nil)
 		lg.GetLogger(ctx).WithError(err).Error("get event")
 		return nil, err
 	}
 
-	startTime := req.StartTime.AsTime()
+	start := req.StartTime.AsTime()
 	var events []storage.Event
 	var err error
 
 	switch req.Period {
 	case pb.GetEventListPeriod_GET_EVENT_LIST_PERIOD_DAY:
-		events, err = s.storage.ListDay(ctx, startTime)
+		events, err = g.storage.ListDay(ctx, start)
 	case pb.GetEventListPeriod_GET_EVENT_LIST_PERIOD_WEEK:
-		events, err = s.storage.ListWeek(ctx, startTime)
+		events, err = g.storage.ListWeek(ctx, start)
 	case pb.GetEventListPeriod_GET_EVENT_LIST_PERIOD_MONTH:
-		events, err = s.storage.ListMonth(ctx, startTime)
+		events, err = g.storage.ListMonth(ctx, start)
 	default:
-		defaultErr := s.createError(ctx, http.StatusBadRequest, "invalid period", nil)
+		defaultErr := g.createError(ctx, http.StatusBadRequest, "invalid period", nil)
 		lg.GetLogger(ctx).WithError(defaultErr).Error("get event")
 		return nil, defaultErr
 	}
 
 	if err != nil {
 		statusCode := model.DefineStatusCode(err.Error())
-		respErr := s.createError(ctx, statusCode, err.Error(), err)
+		respErr := g.createError(ctx, statusCode, err.Error(), err)
 		lg.GetLogger(ctx).WithError(respErr).Error("get event")
 		return nil, respErr
 	}
 
 	protoEvents := make([]*pb.ProtoEvent, 0, len(events))
 	for _, event := range events {
-		protoEvents = append(protoEvents, s.mapEventToProtoEvent(&event))
+		protoEvents = append(protoEvents, g.mapEventToProtoEvent(&event))
 	}
 
-	lg.GetLogger(ctx).Info("proto events", map[string]any{"protoEvents": protoEvents})
+	//lg.GetLogger(ctx).Info("proto events", map[string]any{"protoEvents": protoEvents})
 
 	return &pb.GetEventListResponse{
 		RequestId: utils.GetRequestID(ctx),
@@ -94,7 +87,25 @@ func (s *GrpcServer) GetEvent(ctx context.Context, req *pb.GetEventListRequest) 
 	}, nil
 }
 
-func (s *GrpcServer) mapEventToProtoEvent(event *storage.Event) *pb.ProtoEvent {
+func (g *GrpcServer) DeleteEvent(ctx context.Context, req *pb.DeleteEventRequest) (*pb.DeleteEventResponse, error) {
+	ctx = utils.SetRequestIdToCtx(ctx)
+	logger := g.logger.With(map[string]any{"requestId": utils.GetRequestID(ctx)})
+	ctx = logger.SetLoggerToCtx(ctx)
+
+	err := g.storage.Delete(ctx, req.GetId())
+	if err != nil {
+		statusCode := model.DefineStatusCode(err.Error())
+		respErr := g.createError(ctx, statusCode, err.Error(), err)
+		lg.GetLogger(ctx).WithError(respErr).Error("delete event")
+		return nil, respErr
+	}
+
+	return &pb.DeleteEventResponse{
+		RequestId: utils.GetRequestID(ctx),
+	}, nil
+}
+
+func (g *GrpcServer) mapEventToProtoEvent(event *storage.Event) *pb.ProtoEvent {
 	if event == nil {
 		return nil
 	}
@@ -109,14 +120,14 @@ func (s *GrpcServer) mapEventToProtoEvent(event *storage.Event) *pb.ProtoEvent {
 	}
 }
 
-func (s *GrpcServer) createError(ctx context.Context, statusCode int, message string, cause error) error {
+func (g *GrpcServer) createError(ctx context.Context, statusCode int, message string, cause error) error {
 	requestId := utils.GetRequestID(ctx)
 	cserr := model.NewCalendarServiceErrorAsIs(statusCode, message, requestId, cause)
-	grpcCode := s.mapStatusToGRPCCode(statusCode)
+	grpcCode := g.mapStatusToGRPCCode(statusCode)
 	return status.Error(grpcCode, cserr.ToJSON())
 }
 
-func (s *GrpcServer) mapStatusToGRPCCode(status int) codes.Code {
+func (g *GrpcServer) mapStatusToGRPCCode(status int) codes.Code {
 	switch status {
 	case 400:
 		return codes.InvalidArgument
