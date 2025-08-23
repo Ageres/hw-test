99c30a9da7a32dffc3d1ba802124625f7e37f0fc
sbarmin <sbarmin@beeline.ru>
2025-08-23 18:08:43 +0300

    HW13 is completed

--- hw12_13_14_15_16_calendar/internal/app/app.go
+++ hw12_13_14_15_16_calendar/internal/app/app.go
@@ -22,7 +22,7 @@ const (
 	LISTMONTH Status = "Month event list successfully retrieved"
 )
 
-type AppResponse struct {
+type Response struct {
 	Status Status `json:"status" binding:"required"`
 	Result any    `json:"result,omitempty"`
 }
@@ -31,65 +31,65 @@ func New(_ context.Context, storage storage.Storage) *App {
 	return &App{storage: storage}
 }
 
-func (a *App) AddEvent(ctx context.Context, eventRef *storage.Event) (*AppResponse, error) {
+func (a *App) AddEvent(ctx context.Context, eventRef *storage.Event) (*Response, error) {
 	respEventRef, err := a.storage.Add(ctx, eventRef)
 	if err != nil {
 		return nil, err
 	}
-	return &AppResponse{
+	return &Response{
 		Status: ADD,
 		Result: respEventRef,
 	}, err
 }
 
-func (a *App) UpdateEvent(ctx context.Context, eventRef *storage.Event) (*AppResponse, error) {
+func (a *App) UpdateEvent(ctx context.Context, eventRef *storage.Event) (*Response, error) {
 	err := a.storage.Update(ctx, eventRef)
 	if err != nil {
 		return nil, err
 	}
-	return &AppResponse{
+	return &Response{
 		Status: UPDATE,
 	}, err
 }
 
-func (a *App) DeleteEvent(ctx context.Context, eventID string) (*AppResponse, error) {
+func (a *App) DeleteEvent(ctx context.Context, eventID string) (*Response, error) {
 	err := a.storage.Delete(ctx, eventID)
 	if err != nil {
 		return nil, err
 	}
-	return &AppResponse{
+	return &Response{
 		Status: DELETE,
 	}, err
 }
 
-func (a *App) ListDayEvents(ctx context.Context, startDay time.Time) (*AppResponse, error) {
+func (a *App) ListDayEvents(ctx context.Context, startDay time.Time) (*Response, error) {
 	respEvents, err := a.storage.ListDay(ctx, startDay)
 	if err != nil {
 		return nil, err
 	}
-	return &AppResponse{
+	return &Response{
 		Status: LISTDAY,
 		Result: respEvents,
 	}, err
 }
 
-func (a *App) ListWeekEvents(ctx context.Context, startDay time.Time) (*AppResponse, error) {
+func (a *App) ListWeekEvents(ctx context.Context, startDay time.Time) (*Response, error) {
 	respEvents, err := a.storage.ListWeek(ctx, startDay)
 	if err != nil {
 		return nil, err
 	}
-	return &AppResponse{
+	return &Response{
 		Status: LISTWEEK,
 		Result: respEvents,
 	}, err
 }
 
-func (a *App) ListMonthEvents(ctx context.Context, startDay time.Time) (*AppResponse, error) {
+func (a *App) ListMonthEvents(ctx context.Context, startDay time.Time) (*Response, error) {
 	respEvents, err := a.storage.ListMonth(ctx, startDay)
 	if err != nil {
 		return nil, err
 	}
-	return &AppResponse{
+	return &Response{
 		Status: LISTMONTH,
 		Result: respEvents,
 	}, err
