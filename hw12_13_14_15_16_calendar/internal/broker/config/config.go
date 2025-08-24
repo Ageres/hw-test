74fcdf38215a0f4760c34f8289655f75f0333a57
sbarmin <sbarmin@beeline.ru>
2025-08-24 15:43:56 +0300

    HW14 is completed

---
+++ hw12_13_14_15_16_calendar/internal/broker/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+import (
+	broker "github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/broker"
+	"github.com/Ageres/hw-test/hw12_13_14_15_calendar/internal/model"
+)
+
+func NewBroker(conf *model.BrokerConf) broker.Broker {
+	return nil
+}
