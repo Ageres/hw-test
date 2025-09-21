524f3c7e4d30f24e4a8fbc5935121d58a8ba5d6f
sbarmin <sbarmin@beeline.ru>
2025-09-21 20:39:25 +0300

    HW15 is completed

--- hw12_13_14_15_16_calendar/migrations/0008_init_tab_proc_events.sql
+++ hw12_13_14_15_16_calendar/migrations/0008_init_tab_proc_events.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE notifications (
+CREATE TABLE proc_events (
     id          UUID        PRIMARY KEY
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS notifications;
+DROP TABLE IF EXISTS proc_events;
 -- +goose StatementEnd
