807897dcbf76f516d6c0c39c09f5f6d772cb34b2
sbarmin <sbarmin@beeline.ru>
2025-10-05 22:25:47 +0300

    HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -12,10 +12,40 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+}
+
+// Close implements TelnetClient.
+func (t *telnetClient) Close() error {
+	panic("unimplemented")
+}
+
+// Connect implements TelnetClient.
+func (t *telnetClient) Connect() error {
+	panic("unimplemented")
+}
+
+// Receive implements TelnetClient.
+func (t *telnetClient) Receive() error {
+	panic("unimplemented")
+}
+
+// Send implements TelnetClient.
+func (t *telnetClient) Send() error {
+	panic("unimplemented")
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
 
-	return nil
+	client := telnetClient{
+		address: address,
+		timeout: timeout,
+	}
+
+	return &client
 }
 
 // Place your code here.
