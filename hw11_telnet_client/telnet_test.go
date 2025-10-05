09edcf52ade81fab219281e890246a78e437e8f7
sbarmin <sbarmin@beeline.ru>
2025-10-06 01:24:11 +0300

    HW11 is completed

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -73,7 +73,7 @@ func TestTelnetClient(t *testing.T) {
 
 func TestTelnetClientTimeout(t *testing.T) {
 	t.Run("timeout", func(t *testing.T) {
-		client := NewTelnetClient("someHost:8080", 1*time.Second, io.NopCloser(&bytes.Buffer{}), &bytes.Buffer{})
+		client := NewTelnetClient("someHost.local:8080", 1*time.Second, io.NopCloser(&bytes.Buffer{}), &bytes.Buffer{})
 		err := client.Connect()
 		require.Error(t, err)
 		require.Equal(t, err.Error(), "dial tcp: lookup someHost: i/o timeout")
