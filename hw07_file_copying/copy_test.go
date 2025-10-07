39f8057b43edfad18a3d8b662beac85c64fe3482
sbarmin <sbarmin@beeline.ru>
2025-10-07 04:45:34 +0300

    HW07 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,96 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tmpDir := os.TempDir()
+
+	tests := []struct {
+		name        string
+		from        string
+		to          string
+		offset      int64
+		limit       int64
+		wantErr     bool
+		errType     error
+		compareWith string
+	}{
+		/*{ //OK
+			name:        "offset0_limit0",
+			from:        "testdata/input.txt",
+			to:          filepath.Join(tmpDir, "test1.txt"),
+			offset:      0,
+			limit:       0,
+			wantErr:     false,
+			compareWith: "testdata/out_offset0_limit0.txt",
+		},*/
+		{
+			name:        "offset0_limit10",
+			from:        "testdata/input.txt",
+			to:          filepath.Join(tmpDir, "test2.txt"),
+			offset:      0,
+			limit:       10,
+			wantErr:     false,
+			compareWith: "testdata/out_offset0_limit10.txt",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				if _, err := os.Stat(tt.to); err == nil {
+					os.Remove(tt.to)
+				}
+			}()
+
+			err := Copy(tt.from, tt.to, tt.offset, tt.limit)
+
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("Copy error: nil")
+				}
+				if tt.errType != nil && !errors.Is(err, tt.errType) {
+					t.Errorf("Copy error:  %v, want %v", err, tt.errType)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Copy unexpected error: %v", err)
+					return
+				}
+
+				if _, err := os.Stat(tt.to); os.IsNotExist(err) {
+					t.Errorf("Copy error: destination file not created")
+					return
+				}
+
+				if tt.compareWith != "" {
+					expected, err := os.ReadFile(tt.compareWith)
+					if err != nil {
+						t.Errorf("Read expected file error: %v", err)
+						return
+					}
+
+					actual, err := os.ReadFile(tt.to)
+					if err != nil {
+						t.Errorf("Read actual file error: %v", err)
+						return
+					}
+
+					if string(actual) != string(expected) {
+						//fmt.Println("--------------------------------------------")
+						//fmt.Println(actual)
+						//fmt.Println("--------------------------------------------")
+						//fmt.Println(expected)
+						//fmt.Println("--------------------------------------------")
+						t.Errorf("Copy content mismatch")
+					}
+				}
+			}
+		})
+	}
 }
